8ab4a16cfe0718e84fc51ee6bdc19b93c4e51059
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-02-14 08:22:08 -0300

    reescrevendo as tasks do gulp no formato do gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,76 @@
 /*
+    O objetivo dessas tasks é facilitar o estudo ou o desenvolvimento do front-end: 
+    - pois automatiza algumas tarefas como o reload da página em 
+    vários navegadores e dispositivos cada vez que um arquivo .html e .js é salvo; 
+    - mantém o foco no local da página que está sendo estilizado; 
+    - já vem com um pré-processador de sass configurado 
+    -  
+    
+
     var paths = {}
     - objeto que contem os caminhos das pastas do projeto
     - o objetivo é facilitar a mudança dessas pastas quando esse conjunto de
     tasks for utilizado em outro projeto.
 
+    nomeDaPasta/\**\/*.scss
+    - qualquer arquivo scss dento de nomeDaPasta
+
+    Referencias:
+    - https://www.youtube.com/watch?v=QgMQeLymAdU
+
 */
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    sassCompiler = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
 var paths = {
 
   baseDirServer:'./app',
-  project:'app/**/*',
+  project:'./app/**/*',
+  html: './app/**/*.html',
+  js:'./app/js/**/*.js',
 
-  styles: {
-    src: 'app/css/**/*.css',
-    dest: 'assets/css/'
+  sass:{
+    src: './app/scss/**/*.scss'
   },
-
-  scripts: {
-    src: 'app/js/**/*.js',
-    dest: 'assets/js/'
+  css: {
+    src: './app/css/**/*.css',
+    dest: './app/css/'
   }
 
 };
 
-// Static server
-gulp.task('server', function() {
 
-    browserSync.init({
+function compileSassToCss(){
+  // 1. onde está o arquivo Scss
+  // 2. Passar o arquivo SASS pelo compilador
+  // 3. Onde salvar o css compilado
+  // 4. perpeuar alterações para todos os browsers. Apos configurar o browserSync
+  // -  o uso do metodo browserSync.stream() permite atualização do CSS sem
+  // recarrgar a páginas mantendo o foco no local da que etá sendo estilizado.
+
+  return gulp.src(paths.sass.src)
+    .pipe(sassCompiler())
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(browserSync.stream())
+}
+
+function watch(){
+  // 1. Iniciar o brownserSync
+
+  browserSync.init({
         server: {
             baseDir: paths.baseDirServer
         }
-    });
+   });
 
-    gulp.watch(paths.project).on('change', browserSync.reload);
-});
+   gulp.watch(paths.sass.src, compileSassToCss);
+   gulp.watch(paths.html).on('change', browserSync.reload);
+   gulp.watch(paths.js).on('change', browserSync.reload);
+
+}
+
+// possibilita chamar esta task via linha de comando
+exports.compileScssToCss = compileSassToCss;
+exports.watch = watch;
